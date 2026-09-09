2951a8af1814549f271752b658d7741754c2ad38
Mike <maflores86@gmail.com>
2026-09-08 23:50:05 -0300

    programar comportamiento del splash

--- app/src/main/java/com/ispc/holamundo/SplashActivity.java
+++ app/src/main/java/com/ispc/holamundo/SplashActivity.java
@@ -1,8 +1,10 @@
 package com.ispc.holamundo;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +12,22 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        // Temporizador para cambiar de pantalla automáticamente
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Crear el Intent explícito hacia LoginActivity
+                Intent intent = new Intent(
+                        SplashActivity.this,
+                        LoginActivity.class
+                );
+                startActivity(intent);
+
+                // Cerrar SplashActivity para que el usuario
+                // no vuelva a ella con el botón Atrás
+                finish();
+            }
+        }, 2500); // Tiempo en milisegundos
     }
 }
