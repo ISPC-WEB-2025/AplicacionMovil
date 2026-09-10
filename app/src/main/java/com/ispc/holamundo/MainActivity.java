4353b41c681ad8708f4b1030d4dbf20342e5bd5f
aylenbl <aylen.pdf@gmail.com>
2026-09-09 21:59:42 -0300

    Cambio nombre del paquete: de todostock a holamundo

--- app/src/main/java/com/ispc/holamundo/MainActivity.java
+++ app/src/main/java/com/ispc/holamundo/MainActivity.java
@@ -1,4 +1,4 @@
-package com.ispc.todostock;
+package com.ispc.holamundo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
--- app/src/test/java/com/ispc/holamundo/ExampleUnitTest.java
+++ app/src/test/java/com/ispc/holamundo/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.ispc.todostock;
+package com.ispc.holamundo;
 
 import org.junit.Test;
 
